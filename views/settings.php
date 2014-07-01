bb097dcc5d7636db21c0682317428638fe287e0b
Zane <zane@zanematthew.com>
2014-07-01 12:28:29 -0400

    Adding translation functions, #21

--- views/settings.php
+++ views/settings.php
@@ -14,10 +14,10 @@ $style = get_option( 'ajax_login_register_default_style' );
     <div id="icon-options-general" class="icon32"><br></div>
     <h2><?php _e( 'AJAX Login &amp; Register Settings', 'ajax_login_register' );?></h2>
     <h3>Usage</h3>
-    <p>To create a login page using shortcode; add the following shortcode <code>ajax_login</code> for the login page or <code>ajax_register</code> for the registration page to any Page or Post.</p>
-    <p>To create a login or registration dialog box do the following; create a menu item, assign a custom class name, then add that custom class name to the field: <em>Login Handle</em> for login or <em>Register Handle</em> for the registration page found in the settings below.</p>
-    <p><em>Note your theme must support custom menus</em></p>
-    <p><em>Note your site will need to be open to registration</em></p>
+    <?php _e('<p>To create a login page using shortcode; add the following shortcode <code>ajax_login</code> for the login page or <code>ajax_register</code> for the registration page to any Page or Post.</p>', 'ajax_login_register'); ?>
+    <?php _e('<p>To create a login or registration dialog box do the following; create a menu item, assign a custom class name, then add that custom class name to the field: <em>Login Handle</em> for login or <em>Register Handle</em> for the registration page found in the settings below.</p>', 'ajax_login_register'); ?>
+    <?php _e('<p><em>Note your theme must support custom menus</em></p>', 'ajax_login_register'); ?>
+    <?php _e('<p><em>Note your site will need to be open to registration</em></p>', 'ajax_login_register'); ?>
 
     <form action="options.php" method="post" class="form newsletter-settings-form">
 
