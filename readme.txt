6562185f7b8ce5f353cfb46c3c7e5140e57c9a80
Zane <zane@zanematthew.com>
2014-11-13 10:50:11 -0500

    Adding placeholder and updating language files, #88

--- readme.txt
+++ readme.txt
@@ -159,6 +159,7 @@ Your seeing this message because your site is not open for registration. In orde
 
 = 1.0.9 =
 
+ * Enhancement: Adding placeholder values for front end forms, [88](https://github.com/zanematthew/zm-ajax-login-register/issues/88)
  * Translation: Adding German translation, via Jonas Meier
 
 = 1.0.8 =
--- views/register-form.php
+++ views/register-form.php
@@ -28,13 +28,13 @@
                     <div class="ajax-login-register-status-container">
                         <div class="ajax-login-register-msg-target"></div>
                     </div>
-                    <div class="noon"><label><?php _e('User Name', 'ajax_login_register'); ?></label><input type="text" required name="login" class="user_login" /></div>
-                    <div class="noon"><label><?php _e('Email', 'ajax_login_register'); ?></label><input type="text" required name="email" class="user_email ajax-login-register-validate-email" /></div>
+                    <div class="noon"><label><?php _e('User Name', 'ajax_login_register'); ?></label><input type="text" required placeholder="<?php _e('User Name', 'ajax_login_register'); ?>" name="login" class="user_login" /></div>
+                    <div class="noon"><label><?php _e('Email', 'ajax_login_register'); ?></label><input type="text" required name="email" class="user_email ajax-login-register-validate-email" placeholder="<?php _e('Email', 'ajax_login_register'); ?>" /></div>
 
                     <?php do_action( 'zm_ajax_login_register_below_email_field' ); ?>
 
-                    <div class="noon"><label><?php _e('Password', 'ajax_login_register'); ?></label><input type="password" required name="password" class="user_password" /></div>
-                    <div class="noon"><label><?php _e('Confirm Password', 'ajax_login_register'); ?></label><input type="password" required name="confirm_password" class="user_confirm_password" /></div>
+                    <div class="noon"><label><?php _e('Password', 'ajax_login_register'); ?></label><input type="password" placeholder="<?php _e('Password', 'ajax_login_register'); ?>" required name="password" class="user_password" /></div>
+                    <div class="noon"><label><?php _e('Confirm Password', 'ajax_login_register'); ?></label><input type="password" required name="confirm_password" class="user_confirm_password" placeholder="<?php _e('Confirm Password', 'ajax_login_register'); ?>"/></div>
 
                     <div class="noon"><a href="#" class="already-registered-handle"><?php echo apply_filters( 'ajax_login_register_already_registered_text', __('Already registered?','ajax_login_register') ); ?></a></div>
                     <div class="button-container">
