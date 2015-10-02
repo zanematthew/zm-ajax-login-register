510111627d1bcaabbf1e67a52d8a9e0983e96783
Zane <zane@zanematthew.com>
2015-10-02 14:49:06 -0400

    Checking if username is set, #135

--- src/ALRCore/ALRLogin.php
+++ src/ALRCore/ALRLogin.php
@@ -209,6 +209,7 @@ Class ALRLogin {
 
         $args = apply_filters( $this->prefix . '_form_params', array(
             'user_login' => sanitize_user( $_POST['zm_alr_login_user_name'] ),
+            'user_login' => empty( $_POST['zm_alr_login_user_name'] ) ? null : sanitize_user( $_POST['zm_alr_login_user_name'] ),
             'password' => $_POST['zm_alr_login_password'],
             'remember' => empty( $_POST['remember'] ) ? false : ture
         ) );
